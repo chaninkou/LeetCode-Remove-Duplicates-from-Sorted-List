5d193f1f4a2fdca3de71fd3f81edcba5a1ee2194
chaninkou <michaelkoulove@yahoo.com>
2017-08-31 23:31:24 -0700

    Learning C++

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nickname = "";
+
+    cout << "Enter your cool name: \n";
+    cin >> nickname;
+
+    cout << "Your awesome name is: " << nickname;
+    return 0;
+}
